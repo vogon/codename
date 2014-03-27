0b5f42d3c6dad763e71ea0fbbe1d958f50eb6d77
Colin <vogon@icculus.org>
2014-03-27 07:36:53 -0700

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+apt-get -y install g\+\+ cmake llvm libconfig\+\+ git
+
+git clone https://github.com/ldc-developers/ldc.git
+
+cd ldc
+git submodule update --init
+
+git checkout release-0.12.1
+
+rm -rf build
+mkdir build && cd build
+cmake ..
+make -j4
+make install
+
+apt-get -y install libzmq-dev
+apt-get -y install liblua5.2-dev
